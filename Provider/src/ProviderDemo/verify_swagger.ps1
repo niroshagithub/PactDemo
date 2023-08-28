6ed90add692645349ccbce2ca7bf97977445b05d
Nirosha Kastala <nirosha.kastala@selfwealth.com.au>
2023-08-28 17:37:18 +1000

    Updated script to stop the api.

--- Provider/src/ProviderDemo/verify_swagger.ps1
+++ Provider/src/ProviderDemo/verify_swagger.ps1
@@ -7,4 +7,4 @@ Write-Host "Running schemathesis test to generate report"
 docker run --net="host" schemathesis/schemathesis:stable run --stateful=links --checks all http://host.docker.internal:5170/swagger/v1/swagger.json > report.txt
 
 Write-Host "Stopping dotnet API"
-#Stop-Process -Id $API_PID -Force
+Stop-Process -Id $API_PID -Force
